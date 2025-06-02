3f462422b571683cc9af81a099e14f7830c21605
OlehHnatenko <sinatorgnatenko93@gmail.com>
2025-06-03 00:10:27 +0300

    q:query

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios  from "axios";
 const API_KEY = "50611483-a84caa89bfe2ebe23d067e8bc";
 const URL = "https://pixabay.com/api/";
 
-const searchParam = new URLSearchParams({key: API_KEY, q: null, image_type: "photo", orientation: "horizontal", safesearch: true });
+const searchParam = new URLSearchParams({key: API_KEY, q: "query", image_type: "photo", orientation: "horizontal", safesearch: true });
 
 
 export const getImagesByQuery = (query) => {
